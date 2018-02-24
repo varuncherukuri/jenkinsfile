2548de899d42bcc7d2b4da17e3406dea6c54f6ce
Ubuntu <ubuntu@ip-172-31-39-169.us-east-2.compute.internal>
2018-02-24 05:30:23 +0000

    varun

--- jenkinsfile.c
+++ jenkinsfile.c
@@ -6,7 +6,6 @@ pipeline {
         stage ('compile stage') {
 
             steps {
-
                 withMaven(maven : 'maven_3_0_5') {
                     sh 'mvn clean compile'
                    }
